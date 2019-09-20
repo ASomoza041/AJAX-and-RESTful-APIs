cb397610d4bc0cfb68a29a7e0cdc8f1b6d893f58
Alex Somoza <asomoza041@west-mec.org>
2019-09-19 18:07:40 -0700

    work update
    Had to go and rename some of the files for project 1.

---
+++ Projects/Project01/project1.js
@@ -0,0 +1,61 @@
+/*  Project 01_11_02
+
+    Author: Alex A Somoza
+    Date:  9-18-19 
+
+    Filename: script.js
+*/
+
+"use strict";
+
+
+var httpRequest = false;
+var entry = "IXIC";
+
+function getRequestObject() {
+    try {
+        httpRequest = new XMLHttpRequest();
+    } catch (requestError) {
+        return false;
+    }
+    alert(httpRequest);
+    return httpRequest;
+}
+
+function stopSubmission(evt) {
+    alert("stopSubmission()");
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    } else {
+        evt.returnValue = false;
+    }
+
+    var form = document.getElementsByTagName("form")[0];
+    if (form.addEventListener) {
+        form.addEventListener("onsubmit", stopSubmission, false);
+        window.addEventListener("load", getRequestObject, false);
+    } else if (form.attachEvent) {
+        form.attachEvent("onsubmit", stopSubmission);
+        window.attachEvent("onload",getRequestObject);
+    }
+}
+
+function getQuote() {
+    alert("getQuote()");
+    if (document.getElementsByTagName("input")[0].value) {
+        entry = document.getElementsByTagName("input")[0].value;
+    }
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+}
+
+
+
+
+// if (window.addEventListener) {
+//     window.addEventListener("load", getRequestObject, false);
+// }
+// else if (window.attachEvent) {
+//     window.attachEvent("onload", getRequestObject);
+// }
